a73100630fbb0cbe0f9b8a546813aa961ec22087
Tryton Van Meer <code@trytonvanmeer.dev>
2025-03-16 14:58:24 -0400

    feat: setup gsettings/dconf override

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,6 +5,9 @@ set -ouex pipefail
 # copy files
 rsync -rvK /ctx/system_files/ /
 
+# run scripts
+/ctx/build_files/gsettings.sh
+
 # cleanup
 /ctx/build_files/cleanup.sh
 
---
+++ build_files/gsettings.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+# test gschema overrides for errors
+mkdir -p /tmp/schema-test
+find /usr/share/glib-2.0/schemas/ -type f ! -name "*.gschema.override" -exec cp {} /tmp/schema-test/ \;
+cp /usr/share/glib-2.0/schemas/zz0-modifications.gschema.override /tmp/schema-test/
+
+glib-compile-schemas --strict /tmp/schema-test
+
+# if there are not errors, proceed with compiling custom override gschema
+glib-compile-schemas /usr/share/glib-2.0/schemas &>/dev/null
+
+systemctl enable dconf-update.service
