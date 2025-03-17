254b11fa96f53ef7080708542c1b60f28682b89d
Tryton Van Meer <code@trytonvanmeer.dev>
2025-03-17 19:41:25 -0400

    build: explicitly run dnf clean

--- build_files/cleanup.sh
+++ build_files/cleanup.sh
@@ -3,6 +3,8 @@
 set -ouex pipefail
 shopt -s extglob
 
+dnf clean all
+
 rm -rf /tmp/* || true
 find /var/* -maxdepth 0 -type d \! -name cache -exec rm -fr {} \;
 find /var/cache/* -maxdepth 0 -type d \! -name libdnf5 \! -name rpm-ostree -exec rm -fr {} \;
