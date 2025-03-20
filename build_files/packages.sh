8080ed2382a1a3d0aab0b035dccf3567b4bd0ac7
Tryton Van Meer <code@trytonvanmeer.dev>
2025-03-19 22:03:21 -0400

    feat(packages): add copr support

--- build_files/packages.sh
+++ build_files/packages.sh
@@ -2,8 +2,13 @@
 
 set -ouex pipefail
 
+mapfile -t COPR_REPOS < <(jq -r ".copr.[]" /ctx/build_files/packages.json)
 mapfile -t INCLUDED_PACKAGES < <(jq -r ".include.[]" /ctx/build_files/packages.json)
 mapfile -t EXCLUDED_PACKAGES < <(jq -r ".exclude.[]" /ctx/build_files/packages.json)
+
+for copr in "${COPR_REPOS[@]}"; do
+  dnf5 -y copr enable "$copr"
+done
 
 dnf -y remove "${EXCLUDED_PACKAGES[@]}"
 dnf -y install "${INCLUDED_PACKAGES[@]}"
