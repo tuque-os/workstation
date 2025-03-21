59479f062e99a281f08387f45c2e7864475e98b1
Tryton Van Meer <code@trytonvanmeer.dev>
2025-03-20 21:16:41 -0400

    refactor: rename gschema override file

--- build_files/gsettings.sh
+++ build_files/gsettings.sh
@@ -5,7 +5,7 @@ set -ouex pipefail
 # test gschema overrides for errors
 mkdir -p /tmp/schema-test
 find /usr/share/glib-2.0/schemas/ -type f ! -name "*.gschema.override" -exec cp {} /tmp/schema-test/ \;
-cp /usr/share/glib-2.0/schemas/zz0-modifications.gschema.override /tmp/schema-test/
+cp /usr/share/glib-2.0/schemas/zz0-tuque-workstation.gschema.override /tmp/schema-test/
 
 glib-compile-schemas --strict /tmp/schema-test
 
