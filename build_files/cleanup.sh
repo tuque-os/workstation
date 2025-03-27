c3ae42b6036ad2cb4e343ae1dfbea682c83f778f
Tryton Lux <code@trytonlux.ca>
2025-03-26 22:40:34 -0400

    fix: remove non-ascii filepaths to resolve issue in anaconda

--- build_files/cleanup.sh
+++ build_files/cleanup.sh
@@ -4,3 +4,9 @@ set -ouex pipefail
 shopt -s extglob
 
 find /var/* -maxdepth 0 -type d \! -name cache \! -name log -exec rm -fr {} \;
+
+# remove files with non-ascii characters
+# this fixes an issue where anaconda runs ostree deploy with LANG=C
+# https://github.com/blue-build/cli/issues/310
+# https://github.com/JasonN3/build-container-installer/issues/150
+LC_CTYPE=C fd '[^[:print:]]' --exec rm -v
