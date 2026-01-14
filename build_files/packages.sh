14c347a2a25c3bc666fde9da2c13e530f11747d7
Tryton Lux <code@trytonlux.ca>
2026-01-14 17:10:01 -0500

    feat(packages): add other codecs/drivers from rpm fusion

--- build_files/packages.sh
+++ build_files/packages.sh
@@ -6,15 +6,20 @@ mapfile -t COPR_REPOS < <(jq -r ".copr.[]" /ctx/build_files/packages.json)
 mapfile -t INCLUDED_PACKAGES < <(jq -r ".include.[]" /ctx/build_files/packages.json)
 mapfile -t EXCLUDED_PACKAGES < <(jq -r ".exclude.[]" /ctx/build_files/packages.json)
 
-for copr in "${COPR_REPOS[@]}"; do
-  dnf5 -y copr enable "$copr"
-done
-
+# RPM Fusion repo
 dnf install \
   "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
   "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 
-dnf swap ffmpeg-free ffmpeg --allowerasing
+# codecs and hardware acceleration
+dnf -y swap ffmpeg-free ffmpeg --allowerasing
+dnf -y install intel-media-driver
+dnf -y swap mesa-va-drivers mesa-va-drivers-freeworld
+dnf -y swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
+
+for copr in "${COPR_REPOS[@]}"; do
+  dnf5 -y copr enable "$copr"
+done
 
 dnf -y remove "${EXCLUDED_PACKAGES[@]}"
 dnf -y install "${INCLUDED_PACKAGES[@]}"
